919765f0f7123e011b7033b7a0d2b9d3518b67f0
MLienar <57401150+MLienar@users.noreply.github.com>
2021-01-12 18:58:15 +0100

    font size

--- components/home/contentBox.vue
+++ components/home/contentBox.vue
@@ -1,24 +1,27 @@
 <template>
     <div class="content-box">
         <splashText />
+        <songSlider />
     </div>
 </template>
 
 <script>
 
 import splashText from '../home/splashText'
+import songSlider from '../home/songSlider'
 
 export default {
     name: 'contentBox',
     components: {
-        splashText
+        splashText,
+        songSlider,
         }
 }
 </script>
 
 <style lang="scss">
     .content-box {
-        padding: 15vh 0 5vh 15vh;
+        padding: 15vh 0 7vh 15vh;
         display: flex; 
         flex-flow: row nowrap;
         overflow-x: hidden;
---
+++ components/home/songSlider.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="song-slider">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'songSlider'
+}
+</script>
+
+<style>
+
+</style>
